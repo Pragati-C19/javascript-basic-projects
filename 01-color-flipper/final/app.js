951aaedb1bba9e72d48994059227710e3fc18c5e
Pragati Chothe <pragatichothe@gmail.com>
2021-09-05 18:50:39 +0530

    Color Flipper - checking how randomnumber used

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -4,7 +4,7 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {                 //when user click on check me it navigate to addEventListener for click function. here click is a event follow by function in which what will happen after click function mention
   
-  const randomNumber = getRandomNumber();
+  const randomNumber = 1;          // we want randomnumber between 0-3
   // console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];    //it navigate to css file in which body style background element is change into color. and bcoz of this bacground change 
@@ -12,6 +12,6 @@ btn.addEventListener("click", function () {                 //when user click on
   color.textContent = colors[randomNumber];            // textContent is use for display the color name form colors array 
 });
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);     //random function gives value between 0-1. Here we take random value * array length. floor is use for larger numbers 
-}
+//function getRandomNumber() {
+//  return Math.floor(Math.random() * colors.length);     //random function gives value between 0-1. Here we take random value * array length. floor is use for larger numbers 
+//}
