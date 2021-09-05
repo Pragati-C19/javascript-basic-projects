c23a05825ee29fafa778c912c0d89ae982e340d0
Pragati Chothe <pragatichothe@gmail.com>
2021-09-05 19:03:34 +0530

    Color Flipper - checking how floor and random work

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,17 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "yellow", "blue"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {                 //when user click on check me it navigate to addEventListener for click function. here click is a event follow by function in which what will happen after click function mention
   
-  const randomNumber = 1;          // we want randomnumber between 0-3
-  // console.log(randomNumber);
+  const randomNumber = getRandomNumber();          // we want randomnumber between 0-3
+  console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];    //it navigate to css file in which body style background element is change into color. and bcoz of this bacground change 
   
   color.textContent = colors[randomNumber];            // textContent is use for display the color name form colors array 
 });
 
-//function getRandomNumber() {
-//  return Math.floor(Math.random() * colors.length);     //random function gives value between 0-1. Here we take random value * array length. floor is use for larger numbers 
-//}
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length) ;     //random function gives value between 0-1. Here we take random value * array length. floor is use for absolute number
+}
